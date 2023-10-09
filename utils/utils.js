4b1bc2434ad2ddebe2f716191de6a1f6b1acb99e
pasindu-gamarachchi <pgamarachchi@gmail.com>
2023-10-09 12:23:33 -0400

    get live date

--- utils/utils.js
+++ utils/utils.js
@@ -1,89 +1,97 @@
-const { log } = require('winston');
-const logger = require('../utils/logger');
-
+const { log } = require("winston");
+const logger = require("../utils/logger");
 
 const isValidDateFormat = (dateString) => {
-    const dataRegEx = /^\d{4}-\d{2}-\d{2}$/;
-    return dateString.match(dataRegEx);
+	const dataRegEx = /^\d{4}-\d{2}-\d{2}$/;
+	return dateString.match(dataRegEx);
+};
 
-}
+const isValidDateRange = (req) => {
+	if (!req.params.symb) {
+		logger.error(`Missing stock symbol`);
+		return {
+			statusCode: 400,
+			errJson: { isError: true, errMsg: "Missing stock symbol" },
+		};
+	}
 
-const isValidDateRange = (req)=> {
+	if (!req.query.from) {
+		logger.error(`Missing from date`);
+		return {
+			statusCode: 400,
+			errJson: { isError: true, errMsg: "Missing from date" },
+		};
+	} else if (!req.query.to) {
+		logger.error(`Missing to date`);
+		return {
+			statusCode: 400,
+			errJson: { isError: true, errMsg: "Missing to date" },
+		};
+	}
 
-    if (!req.params.symb){
-        logger.error(`Missing stock symbol`);
-        return {
-            statusCode: 400,
-            errJson : {"isError": true, "errMsg": "Missing stock symbol" }
-        }
-    }
+	if (!isValidDateFormat(req.query.from)) {
+		logger.error(`Invalid  from date ${req.query.from}`);
+		return {
+			statusCode: 400,
+			errJson: { isError: true, errMsg: "Invalid from date" },
+		};
+	}
 
-    if (!req.query.from){
-        logger.error(`Missing from date`);
-        return {
-            statusCode: 400,
-            errJson : {"isError": true, "errMsg": "Missing from date" }
-        }
-    }
-    else if (!req.query.to){
-        logger.error(`Missing to date`);
-        return {
-            statusCode: 400, 
-            errJson: {"isError": true, "errMsg": "Missing to date" }
-        }
-    }
+	if (!isValidDateFormat(req.query.to)) {
+		logger.error(`Invalid  to date`);
+		return {
+			statusCode: 400,
+			errJson: { isError: true, errMsg: "Invalid to date" },
+		};
+	}
+	return false;
+};
 
-
-    if (!isValidDateFormat(req.query.from)){
-        logger.error(`Invalid  from date ${req.query.from}`);
-        return {
-            statusCode: 400,
-            errJson: {"isError": true, "errMsg": "Invalid from date" }
-        }
-    }
-
-    if (!isValidDateFormat(req.query.to)){
-        logger.error(`Invalid  to date`);
-        return {
-            statusCode: 400,
-            errJson : {"isError": true, "errMsg": "Invalid to date" }
-        }
-    }
-    return false;
-}
-
-const isValidReqBody = (reqBody, requiredProps) =>{
-    let isValidBody = true;
-    requiredProps.forEach( (el) =>{
-        logger.info(`Validating req col : ${el}, ${el in reqBody}`);
-        if (!(el in reqBody)){
-            isValidBody = false;
-        }
-    }
-    );
-    return isValidBody;
-}
+const isValidReqBody = (reqBody, requiredProps) => {
+	let isValidBody = true;
+	requiredProps.forEach((el) => {
+		logger.info(`Validating req col : ${el}, ${el in reqBody}`);
+		if (!(el in reqBody)) {
+			isValidBody = false;
+		}
+	});
+	return isValidBody;
+};
 
 const isInt = (num) => {
-    if ((String(num).trim()[0] === "+") || (String(num).trim()[0] === "-")) {
-      return false;
-    }
-    return (typeof (num) === 'number' || typeof (num) === "string" && num.trim() !== '') && !isNaN(num);
-  }
+	if (String(num).trim()[0] === "+" || String(num).trim()[0] === "-") {
+		return false;
+	}
+	return (
+		(typeof num === "number" ||
+			(typeof num === "string" && num.trim() !== "")) &&
+		!isNaN(num)
+	);
+};
 
-const isPrice = (num) =>{
-    let regex  = /^\d+(?:\.\d{0,2})$/;
-    if (regex.test(num)){
-        return true;
-    }
-    return false;
-}
+const isPrice = (num) => {
+	let regex = /^\d+(?:\.\d{0,2})$/;
+	if (regex.test(num)) {
+		return true;
+	}
+	return false;
+};
 
+const getToday = () => {
+	const today = new Date();
+	const year = today.getFullYear();
+	const month = String(today.getMonth() + 1).padStart(2, "0");
+	const day = String(today.getDate()).padStart(2, "0");
+
+	const formattedDate = `${year}-${month}-${day}`;
+	return formattedDate;
+};
 
 module.exports = {
-    isValidDateFormat,
-    isValidDateRange,
-    isValidReqBody,
-    isInt,
-    isPrice
-}
+	isValidDateFormat,
+	isValidDateRange,
+	isValidReqBody,
+	isInt,
+	isPrice,
+	getToday,
+};
